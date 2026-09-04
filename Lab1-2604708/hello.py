242fdcc07630fcbfca151286f61cf4d0ed15f4e8
damien-xy <ethelxy.lin@gmail.com>
2026-09-04 14:45:33 +0800

    Update Profile

--- Lab1-2604708/hello.py
+++ Lab1-2604708/hello.py
@@ -2,3 +2,12 @@ print("===============================")
 print("Welcome here!")
 print("My first post!")
 print("===============================")
+
+# activity 2 - update profile
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
