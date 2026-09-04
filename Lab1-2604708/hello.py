9ba7c266fb9062d9ca84198ca4e77f1661f30202
damien-xy <ethelxy.lin@gmail.com>
2026-09-04 14:59:31 +0800

    Added if statement

--- Lab1-2604708/hello.py
+++ Lab1-2604708/hello.py
@@ -1,9 +1,11 @@
+print("--------- Activity 1 ---------")
 print("===============================")
 print("Welcome here!")
 print("My first post!")
 print("===============================")
 
 # activity 2 - update profile
+print("\n--------- Activity 2 ---------")
 username = "cool_creator"
 bio = "Fun Blogger"
 followers = 100
@@ -13,6 +15,7 @@ print("Bio:", bio)
 print("Followers:", followers)
 
 # activity 3 - add follower growth tracker
+print("\n--------- Activity 3 ---------")
 followers = 100
 
 followers += 50
@@ -25,6 +28,7 @@ followers += 10
 print("Day 3: ", followers)
 
 # activity 4 = interactive profile creator
+print("\n--------- Activity 4 ---------")
 username = input("Enter Username: ")
 age = input("Enter Age: ")
 category = input("Enter Content Category: ")
@@ -34,3 +38,19 @@ print("================================")
 print("Username: ", username)
 print("Age: ", age)
 print("Content Category: ", category)
+
+# activity 5 - added if statement
+print("\n--------- Activity 5 ---------")
+username = input("Enter Username: ")
+age = int(input("Enter Age: "))
+category = input("Enter Content Category: ")
+
+print("\nInstagram Profile")
+print("================================")
+print("Username: ", username)
+print("Age: ", age)
+print("Content Category: ", category)
+
+if age > 40 and category == "fun":
+    print("You are old what is fun for you??")
+
